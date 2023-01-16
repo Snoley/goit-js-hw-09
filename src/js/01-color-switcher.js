4fdf5f4aedc6f5ecb51dff9224bf51d2dab6f786
Gleb <zimagleb@gmail.com>
2023-01-16 13:31:52 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+let intervalId;
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  const body = document.querySelector('body');
 
+  startBtn.addEventListener('click', function() {
+    startBtn.disabled = true;
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+
+  stopBtn.addEventListener('click', function() {
+    startBtn.disabled = false;
+    clearInterval(intervalId);
+  });
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
