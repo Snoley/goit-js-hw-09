867fb0498c823490df80435f49a46eb0627ae68b
Snoley <110094116+Snoley@users.noreply.github.com>
2023-01-16 11:32:15 +0000

    Deploying to gh-pages from @ Snoley/goit-js-hw-09@4fdf5f4aedc6f5ecb51dff9224bf51d2dab6f786 🚀

---
+++ 01-color-switcher.0f7b7d00.js
@@ -0,0 +1,2 @@
+let e;const t=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){t.disabled=!0,e=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),o.addEventListener("click",(function(){t.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.0f7b7d00.js.map
